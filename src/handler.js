fa79b17fa1f3d30239e117cf95bbedccc379b82f
Niel Pasae <sjrnl27@gmail.com>
2023-03-19 10:47:44 +0800

    fix query problems

--- src/handler.js
+++ src/handler.js
@@ -81,11 +81,11 @@ const getAllBookHandlerById = (request, h) => {
   }
 
   if (reading !== undefined) {
-    allBook = allBook.filter((book) => book.reading === reading);
+    allBook = allBook.filter((book) => Boolean(book.reading) === Boolean(reading));
   }
 
   if (finished !== undefined) {
-    allBook = allBook.filter((book) => book.finished === finished);
+    allBook = allBook.filter((book) => Boolean(book.finished) === Boolean(finished));
   }
 
   allBook = allBook.map((book) => ({
